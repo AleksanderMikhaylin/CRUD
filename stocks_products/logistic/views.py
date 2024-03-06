03f5f7ceddc503b89c579b6e44c94ee191cb18f2
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-03-06 21:05:06 +1100

    Add files via upload

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,22 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+from models import Product, Stock
+from serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_fields = ['title', 'description']
+    search_fields = ['title', 'description']
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_fields = ['address', 'products']
+    search_fields = ['address', 'products']
+    # при необходимости добавьте параметры фильтрации
