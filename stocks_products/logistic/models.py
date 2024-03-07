a76146950fcd72cc41631f9315ddbf6b3db78134
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-03-07 13:01:05 +1100

    Update models.py

--- stocks_products/logistic/models.py
+++ stocks_products/logistic/models.py
@@ -6,8 +6,12 @@ class Product(models.Model):
     title = models.CharField(max_length=60, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Stock(models.Model):
+    name = models.CharField(max_length=50)
     address = models.CharField(max_length=200, unique=True)
     products = models.ManyToManyField(
         Product,
