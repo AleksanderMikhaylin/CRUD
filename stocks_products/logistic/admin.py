e2802ea87343fe7f73773087bb27f1d5543ef307
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-03-07 13:00:43 +1100

    Update admin.py

--- stocks_products/logistic/admin.py
+++ stocks_products/logistic/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Stock, StockProduct
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+        list_display = ['title', 'description']
+
+class StockProductInLine(admin.TabularInline):
+    model = StockProduct
+    list_display = ['product', 'quantity', 'price']
+    extra = 0
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+        list_display = ['name', 'address']
+        inlines = [StockProductInLine, ]
