933830085b941a1cdd865018113d2c2256a5e26f
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-03-14 11:01:37 +1100

    Update views.py

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -1,22 +1,22 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
-from .models import Product, Stock
-from .serializers import ProductSerializer, StockSerializer
-
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.pagination import LimitOffsetPagination
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    filter_fields = ['title', 'description']
-    search_fields = ['title', 'description']
+    search_fields = ['description',]
+    filterset_fields = ['title',]
+    pagination_class = LimitOffsetPagination
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    filter_fields = ['address', 'products']
-    search_fields = ['address', 'products']
-    # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products',]
+    search_fields = ['positions__product__title']
+    pagination_class = LimitOffsetPagination
